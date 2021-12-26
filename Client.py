a653097f990dbd1946ea29c5c7ba991505346830
danrotSavage <danrot@post.bgu.ac.il>
2021-12-26 16:59:31 +0200

    new client

--- Client.py
+++ Client.py
@@ -1,39 +1,53 @@
 import os
+import signal
 import socket
 import struct
 import time
+import sys
+import select
 from threading import Thread
-from termcolor import colored
+
+# import getch
 
 os.system("")
 
 UDP_PORT = 13117
 MESSAGE_LEN = 1024
-GAME_TIME = 10
+GAME_TIME = 10  # seconds
 BROADCAST_IP = ""
 sockUDP = None
 sockTCP = None
 printBool = True
+
 end = time.time()
 
 
-# Handle signal to escape blocking thread
-def handler(signum, frame):
-    raise Exception()
+# Group of Different functions for different styles
+class style:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
 
-
-def print_server_message():
-    while printBool:
-        time.sleep(0.1)
-        try:
+def printMessageOrRead():
+    readers, _, _ = select.select([sys.stdin, sockTCP], [], [])
+    for reader in readers:
+        if reader is sockTCP:
             data = sockTCP.recv(MESSAGE_LEN).decode()
-            print(colored(data, 'green'))
-        except Exception as e:
-            print(e)
-            break
+            print (data)
+        else:
+            ch = sys.stdin.readline()
+            sockTCP.sendall(ch.encode())
+            printMessageOrRead() #because will need to read server answer
 
 
-print(colored("Client started, listening for offer requests...", 'green'))  # waits for server suggestion
+print("Client started, listening for offer requests...")  # waits for server suggestion
 
 
 def start_udp():
@@ -47,48 +61,150 @@ def start_udp():
     return server_ip_address, message
 
 
-def start_tcp(ip='127.0.0.1', tcp_port='2004'):
+def start_tcp(ip, tcp_port):
     global sockTCP
     sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # init TCP socket
     sockTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sockTCP.connect((ip, tcp_port))
 
 
-def assign_thread():
-    printer = Thread(target=print_server_message, args=())
-    printer.start()
+
+while True:
+
+    #part 1 get udp message
+    ip, message = start_udp()
+    try:
+
+        #part 2 connect to serer
+        printBool = True
+        cookie, msg_type, tcp_port = struct.unpack('LBH', message)  # get message in specific format
+
+        #part 3 make sure its the corrrect server
+        if cookie == 0xabcddcba and msg_type == 0x2:  # check if message is as expected
+            print("Received offer from " + ip + " attempting to connect...")
+            start_tcp(ip, tcp_port)
+
+            #part 4 start game with group name
+            group_name = raw_input('Enter your group name: ')
+            print(group_name)
+            sockTCP.sendall(group_name.encode())  # send team's name to server
+
+            #part 5 start game
+            print(sockTCP.recv(MESSAGE_LEN).decode())  # the game begin message
+            print(sockTCP.recv(MESSAGE_LEN).decode())  # math question
+
+            #part 6 play game, win or lost
+            printMessageOrRead()
+
+            print("Server disconnected, listening for offer requests...")
+
+
+        else:
+            print("Bad UDP Message Format")  # got message not in the expected format
+    except Exception as e:
+        print(e)
+import os
+import signal
+import socket
+import struct
+import time
+import sys
+import select
+from threading import Thread
+
+# import getch
+
+os.system("")
+
+UDP_PORT = 13117
+MESSAGE_LEN = 1024
+GAME_TIME = 10  # seconds
+BROADCAST_IP = ""
+sockUDP = None
+sockTCP = None
+printBool = True
+
+end = time.time()
+
+
+# Group of Different functions for different styles
+class style:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+def printMessageOrRead():
+    readers, _, _ = select.select([sys.stdin, sockTCP], [], [])
+    for reader in readers:
+        if reader is sockTCP:
+            data = sockTCP.recv(MESSAGE_LEN).decode()
+            print (data)
+        else:
+            ch = sys.stdin.readline()
+            sockTCP.sendall(ch.encode())
+            printMessageOrRead() #because will need to read server answer
+
+
+print("Client started, listening for offer requests...")  # waits for server suggestion
+
+
+def start_udp():
+    global sockUDP
+    sockUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # init UDP socket
+    sockUDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sockUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sockUDP.bind((BROADCAST_IP, UDP_PORT))
+    message, location = sockUDP.recvfrom(MESSAGE_LEN)
+    server_ip_address = str(location[0])
+    return server_ip_address, message
+
+
+def start_tcp(ip, tcp_port):
+    global sockTCP
+    sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # init TCP socket
+    sockTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sockTCP.connect((ip, tcp_port))
+
 
 
 while True:
+
+    #part 1 get udp message
     ip, message = start_udp()
     try:
+
+        #part 2 connect to serer
         printBool = True
         cookie, msg_type, tcp_port = struct.unpack('LBH', message)  # get message in specific format
+
+        #part 3 make sure its the corrrect server
         if cookie == 0xabcddcba and msg_type == 0x2:  # check if message is as expected
-            print(colored("Received offer from " + ip + " attempting to connect...", 'green'))
+            print("Received offer from " + ip + " attempting to connect...")
             start_tcp(ip, tcp_port)
-            group_name = input('Enter your group name: ')
+
+            #part 4 start game with group name
+            group_name = raw_input('Enter your group name: ')
             print(group_name)
-            # it will wait in socket until server recv it
             sockTCP.sendall(group_name.encode())  # send team's name to server
-            msg = sockTCP.recv(MESSAGE_LEN).decode()
-            print(colored(msg, 'pink'))
-            msg2 = sockTCP.recv(MESSAGE_LEN).decode()
-            print(colored(msg2, 'pink'))  # math question
-            assign_thread()  # end game
-            while True:
-                try:
-                    ch = input("enter char answer")
-                    print(ch)
-                    sockTCP.sendall(ch.encode())  # if socket is still open, send it
-                    time.sleep(0.2)  # wait for server to answer
-                    printBool = False
-                    break
-                except Exception as e:
-                    print(e)
-                    break
+
+            #part 5 start game
+            print(sockTCP.recv(MESSAGE_LEN).decode())  # the game begin message
+            print(sockTCP.recv(MESSAGE_LEN).decode())  # math question
+
+            #part 6 play game, win or lost
+            printMessageOrRead()
+
+            print("Server disconnected, listening for offer requests...")
+
 
         else:
-            print(colored("Bad UDP Message Format", 'red'))  # got message not in the expected format
+            print("Bad UDP Message Format")  # got message not in the expected format
     except Exception as e:
-        print(colored(str(e), 'red'))
+        print(e)
