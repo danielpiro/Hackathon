e7f54525e7e4d1f6e7973f1bdaa80750589415a6
daniel piro <danielpiro2@gmail.com>
2021-12-26 16:47:34 +0200

    fixing color bugs and threads

--- Server.py
+++ Server.py
@@ -5,22 +5,14 @@ from random import randint
 from threading import Thread
 import os
 
-# from scapy.arch import get_if_addr
-
 os.system("")
 
 
 # Group of Different functions for different styles
-class style:
-    BLACK = '\033[30m'
-    RED = '\033[31m'
+class Colors:
     GREEN = '\033[32m'
-    YELLOW = '\033[33m'
     BLUE = '\033[34m'
     MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
 
@@ -42,7 +34,7 @@ CONN_A = None
 CONN_B = None
 A_WON = False
 counter = 0
-amountOfPlayers= 1
+amountOfPlayers = 1
 answer = -1
 gotAnswer = False
 
@@ -60,10 +52,10 @@ def start_udp():
 def send_broadcast():
     global sockUDP
     ip = start_udp()
-    print("Server started, listening on IP address " + ip)
+    print(Colors.GREEN + "Server started, listening on IP address " + ip)
     time_out = time.time() + TIME_TO_CONNECT  # the time to wait for players
     while True:
-        if counter<amountOfPlayers:             # time.time() < time_out:
+        if counter < amountOfPlayers:  # time.time() < time_out:
             buffer = struct.pack('LBH', 0xabcddcba, 0x2, TCP_DEST_PORT)
             sockUDP.sendto(buffer, (UDP_DEST_IP, UDP_DEST_PORT))
             time.sleep(0.5)  # wait 1 sec
@@ -76,7 +68,7 @@ def connect_clients():
     time_out = time.time() + TIME_TO_CONNECT  # the time to wait for players
     sockTCP.settimeout(TIME_TO_CONNECT)
     while True:
-        if counter<amountOfPlayers:                 #time.time() < time_out and counter < 2:
+        if counter < amountOfPlayers:  # time.time() < time_out and counter < 2:
             try:
                 conn, address = sockTCP.accept()
 
@@ -84,20 +76,21 @@ def connect_clients():
                     IP_A = address[0]
                     PORT_A = address[1]
                     CONN_A = conn
-                    print ("connected new client 1 " + IP_A)
+                    print(Colors.BLUE + "connected new client 1 " + IP_A)
                 else:
                     IP_B = address[0]
                     PORT_B = address[1]
                     CONN_B = conn
-                    print ("connected new client 2 " + IP_A)
+                    print(Colors.MAGENTA + "connected new client 2 " + IP_A)
 
                 counter += 1
-                print("New player connected with ip: " + address[0] + " and with port " + str (address[1]))
+                print(
+                    Colors.GREEN + "New player connected with ip: " + address[0] + " and with port " + str(address[1]))
             except Exception as e:
                 pass
 
         else:
-            print("game starts in 10 secs")
+            print(Colors.BLUE + "game starts in 10 secs")
             time.sleep(TIME_TO_PLAY)
             break
 
@@ -108,9 +101,9 @@ def get_group_names():
         NAME_A = CONN_A.recv(MESSAGE_LENGTH).decode()
         print(NAME_A)
         NAME_B = CONN_B.recv(MESSAGE_LENGTH).decode()
-        print (NAME_B)
+        print(NAME_B)
     except Exception as e:
-        print ("group name was not entered so could'nt start the game")
+        print(Colors.GREEN + "group name was not entered so couldn't start the game")
 
 
 def send_message_to_group(message, group):
@@ -118,6 +111,7 @@ def send_message_to_group(message, group):
         try:
             key.sendall(message)
         except Exception as e:
+            print(e)
             pass
 
 
@@ -126,13 +120,14 @@ def send_message(message):
         CONN_A.sendall(message.encode())
         CONN_B.sendall(message.encode())
     except Exception as e:
+        print(e)
         pass
 
 
 def receive_char():
-    global A_WON, gotAnswer,CONN_A,CONN_B
-    #CONN_A.setblocking(0)
-    #CONN_B.setblocking(0)
+    global A_WON, gotAnswer, CONN_A, CONN_B
+    # CONN_A.setblocking(0)
+    # CONN_B.setblocking(0)
     gotAnswer = False
     timeout = time.time() + TIME_TO_PLAY
     while time.time() < timeout and gotAnswer == False:
@@ -145,7 +140,7 @@ def receive_char():
             else:
                 A_WON = False
             gotAnswer = True
-            print("got data from A, A's answer is " + str(data) + " but the real answer is " + str(answer))
+            print(Colors.BLUE + "got data from A, A's answer is " + str(data) + " but the real answer is " + str(answer))
         except:
             try:
                 data = CONN_B.recv(1024)
@@ -159,7 +154,6 @@ def receive_char():
                 time.sleep(0.1)
 
 
-
 def total_messages(group):
     m = ""
     for c in group.keys():
@@ -167,22 +161,23 @@ def total_messages(group):
             c.settimeout(0)
             m += c.recv(MESSAGE_LENGTH).decode()
         except Exception as e:
+            print(e)
             pass
     return m
 
 
 def send_end_message():
-    winner_group = ''
     if A_WON:
         winner_group = NAME_A
     else:
         winner_group = "No Name"  # NAME_B
 
     if gotAnswer:
-        end_message = "Game over!\nThe correct answer was " + str(answer) + "!\nCongratulations to the winner:" + winner_group
+        end_message = "Game over!\nThe correct answer was " + str(
+            answer) + "!\nCongratulations to the winner:" + winner_group
     else:
         end_message = "Game over!\nNo one answered - Draw"
-    print (end_message)
+    print(Colors.MAGENTA + end_message)
     send_message(end_message)
 
 
@@ -219,9 +214,8 @@ def send_math_question():
 
 
 def start_game():
-
     get_group_names()
-    begin_message = "Welcome to Quick Maths.\nPlayer 1: "+ NAME_A + "\nPlayer 2: " + NAME_B +"\n====\n Please answer the following question as fast as you can:\n"
+    begin_message = "Welcome to Quick Maths.\nPlayer 1: " + NAME_A + "\nPlayer 2: " + NAME_B + "\n====\n Please answer the following question as fast as you can:\n"
 
     send_message(begin_message)
     send_math_question()
@@ -252,15 +246,14 @@ def main():
         start_game()  # play the game
         print("Starting new Game, sending out offer requests...")  # game session over
         counter = 0
-        A_WON=False
-        gotAnswer=False
-        answer=-1
+        A_WON = False
+        gotAnswer = False
+        answer = -1
         try:
             CONN_A.close()
             CONN_B.close()
         except:
             pass
-
 
 
 if __name__ == "__main__":
